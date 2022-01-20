d7eb539df48638a13d08a5c58e46bf07970df689
[AkenovAV] <[Flarher@yandex.ru]>
2022-01-21 01:07:10 +0300

    Dz 5

---
+++ dz5_N2.py
@@ -0,0 +1,10 @@
+"""
+* (вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
+"""
+
+max_num = 15
+odd_nums = (num for num in range(1, max_num + 1) if num % 2 != 0)
+
+print(next(odd_nums))
+print(next(odd_nums))
+print(next(odd_nums))
